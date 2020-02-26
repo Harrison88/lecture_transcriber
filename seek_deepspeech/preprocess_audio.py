3a6758f7cf5a4ed54a2c409771a470037c99448e
Harrison Morgan <harrison.morgan@gmail.com>
2020-02-25 19:53:55 -0600

    Added silence_threshold calculation to preprocessing

--- seek_deepspeech/preprocess_audio.py
+++ seek_deepspeech/preprocess_audio.py
@@ -13,13 +13,35 @@ def preprocess_audio(audio_filepath, desired_framerate):
     split_audio = audiosegments_to_np(split_audio)
     return split_audio, audio_seconds
 
-def split_on_silence(audio):
-    split_audio = pydub.silence.split_on_silence(audio, silence_thresh=-75, min_silence_len=700)
+def split_on_silence(audio, *, silence_thresh=-75):
+    split_audio = pydub.silence.split_on_silence(audio, silence_thresh=silence_thresh, min_silence_len=700)
     summed_duration = sum(audio.duration_seconds for audio in split_audio)
     print("Summed audio duration (seconds) after split:", summed_duration)
     print("Number of segments:", len(split_audio))
     print("Average segment duration (seconds):", summed_duration / len(split_audio))
     return split_audio
+
+
+def determine_silence_threshold(audio):
+    chunk_size = 50
+    lower_bounds = range(0, len(audio), chunk_size)
+    upper_bounds = range(chunk_size, len(audio)+chunk_size, chunk_size)
+    
+    samples = []
+    for lower_bound, upper_bound in zip(lower_bounds, upper_bounds):
+        segment = audio[lower_bound:upper_bound]
+        samples.append(segment.dBFS)
+    
+    silence = min(samples)
+    average = audio.dBFS
+    loudest = max(samples)
+    
+    print(f"Audio loudness stats: {silence = }; {average = }; {loudest = }")
+    
+    threshold = silence - ((silence - average) * .15)
+    print(f"Threshold calculated to be {threshold} dBFS")
+    
+    return threshold
 
 
 def get_audiosegment(audio_filepath, desired_framerate):
@@ -49,7 +71,8 @@ def main():
     args = parse_args()
     
     audio, audio_seconds = get_audiosegment(args.audio, args.framerate)
-    split_audio = split_on_silence(audio)
+    threshold = determine_silence_threshold(audio)
+    split_audio = split_on_silence(audio, silence_thresh=threshold)
     output_dir = Path(args.output_dir)
     for index, audio_segment in tqdm(enumerate(split_audio)):
         output_path = output_dir / f"{index}.wav"
